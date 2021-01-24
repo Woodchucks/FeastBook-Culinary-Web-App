0d935c5f240c2d1591219f522bd77ea93631720e
Woodchucks <37177696+Woodchucks@users.noreply.github.com>
2021-01-24 18:53:30 +0100

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,12 +11,6 @@ namespace FeastBook_final.Controllers
 {
     public class HomeController : Controller
     {
-        /*private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }*/
         private readonly ApplicationDbContext _context;
         public HomeController(ApplicationDbContext context)
         {
